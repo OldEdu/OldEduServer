0d5a3c3c4adfab958ecc8af885e099a56f5a8f15
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-17 11:37:42 +0900

    [init] 폴더구조 최적화
    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -2,78 +2,85 @@ const express = require("express");
 const bodyParser =require("body-parser");
 const app = express();
 
-const admin=require("firebase-admin");
-const credentials =require("../app/key.json");
+const db=require("./src/config/db");
+//라우팅
+const home =require("./src/routes/home");
 
-admin.initializeApp({
-    credential:admin.credential.cert(credentials)
-})
+// 앱 셋팅
+app.set("views","./src/views");
+app.set("view engine","ejs");
+
 app.use(express.json());
 //URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended:true}));
 
-const db =admin.firestore();
+app.use("/",home);
 
-app.post('/create', async(req,res)=>{
-    try{
-        console.log(req.body);
-        const phoneNumber =req.body.phoneNumber;
-        const userJson={
-            phoneNumber: req.body.phoneNumber,
-            nickName:req.body.nickName,
-            psword:req.body.psword
-        };
-        const response =await db.collection("users").doc(phoneNumber).set(userJson);
-        res.send(response);
-    }catch(error){
-        res.send(error);
-    };
-})
+module.exports =app;
 
-app.get('/read/all',async(req,res)=>{
-    try{
-        const usersRef =db.collection("users");
-        const response =await usersRef.get();
-        let responseArr = [];
-        response.forEach(doc=>{
-            responseArr.push(doc.data());
-        });
-        res.send(responseArr);
-    }catch(error){
-        res.send(error)
-    }
-})
 
-app.get('/read/:phoneNumber',async(req,res)=>{
-    try{
-        const userRef =db.collection("users").doc(req.params.phoneNumber);
-        const response =await userRef.get();
-        res.send(response.data());
-    }catch(error){
-        res.send(error)
-    }
-});
 
-app.post('/update',async(req,res)=>{
-    try{
-        const phoneNumber=req.body.phoneNumber;
-        const newnickName=req.body.nickName;
-        const userRef =await db.collection("users").doc(phoneNumber)
-        .update({
-            nickName:newnickName
-        })
-        res.send(response);
-    }catch(error){
-        res.send(error);
-    }
-})
+// app.post('/create', async(req,res)=>{
+//     try{
+//         console.log(req.body);
+//         const phoneNumber =req.body.phoneNumber;
+//         const userJson={
+//             phoneNumber: req.body.phoneNumber,
+//             nickName:req.body.nickName,
+//             psword:req.body.psword
+//         };
+//         const response =await db.collection("users").doc(phoneNumber).set(userJson);
+//         res.send(response);
+//     }catch(error){
+//         res.send(error);
+//     };
+// })
+
+// app.get('/read/all',async(req,res)=>{
+//     try{
+//         const usersRef =db.collection("users");
+//         const response =await usersRef.get();
+//         let responseArr = [];
+//         response.forEach(doc=>{
+//             responseArr.push(doc.data());
+//         });
+//         res.send(responseArr);
+//     }catch(error){
+//         res.send(error)
+//     }
+// })
+
+// app.get('/read/:phoneNumber',async(req,res)=>{
+//     try{
+//         const userRef =db.collection("users").doc(req.params.phoneNumber);
+//         const response =await userRef.get();
+//         res.send(response.data());
+//     }catch(error){
+//         res.send(error)
+//     }
+// });
+
+// app.post('/update',async(req,res)=>{
+//     try{
+//         const phoneNumber=req.body.phoneNumber;
+//         const newnickName=req.body.nickName;
+//         const userRef =await db.collection("users").doc(phoneNumber)
+//         .update({
+//             nickName:newnickName
+//         })
+//         res.send(response);
+//     }catch(error){
+//         res.send(error);
+//     }
+// })
 
 app.delete('/delete/:phoneNumber',async(req,res)=>{
     try{
+        console.log(res);
         const response =await db.collection("users").doc(req.params.phoneNumber).delete();
         res.send(response);
     }catch(error){
         res.send(error);
     }
 })
-module.exports =app;
+
