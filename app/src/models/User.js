8f6a68cdf9b25014c5d8a160146120ac0565bb22
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-17 11:39:17 +0900

    [FEAT] 회원가입 기능 구현
    - 회원가입 기능 구현
    - User model 구현
    - UserStorage model 구현

---
+++ app/src/models/User.js
@@ -0,0 +1,40 @@
+"use strict";
+/*
+Usesr은 해당 데이터를 가지고 검증 및 조작하는 역할
+*/
+
+const UserStorage =require("./UserStorage");
+
+class User{
+    constructor(body){
+        this.body=body;
+    }
+    //로그인 기능
+    async login(){
+        const client =this.body
+
+        try{
+            const {phoneNumber, psword} = await UserStorage.getUserInfo(client.phoneNumber);
+            if(phoneNumber){
+                if(phoneNumber === client.phoneNumber && psword ===client.psword){
+                    return {success :true};
+                }
+                return {success:false ,msg:"비밀번호가 틀렸습니다."}; 
+            }
+            return {success:false,msg:"존재하지 않는 아이디 입니다."};
+        }catch(err){
+            return {success:false,err};
+        }
+    }
+    //회원가입 기능
+    async register(){
+        const client =this.body;
+        try{
+            const response =await UserStorage.save(client);
+            return response;
+        }catch(err){
+            return {success: false, err};
+        }
+    }
+}
+module.exports=User
