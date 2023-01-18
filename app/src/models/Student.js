1c55aa6b2fdf37b3cd3be55e044f3e2d0e3222ce
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-18 15:26:42 +0900

    [FEAT]학습자 정보 가져오기 API구현 완료
    - StudentStorage에서 phoneNumber로 db에서 학습자 정보 가져오는 getStudentInfo 메소드 생성
    - Student model에서 학습자 정보를 갖고오는 getStudent 메소드 생성

--- app/src/models/Student.js
+++ app/src/models/Student.js
@@ -7,6 +7,16 @@ class Student {
     constructor(body){
         this.body=body;
     }
+    //학습자 정보 갖고 오기
+    async getStudent(){
+        try{
+            const client = this.body;
+            const response = await StudentStorage.getStudentInfo(client);
+            return response;
+        }catch(err){
+            return {success:false, err};
+        }
+    }
     async register(){
         const client = this.body;
         try{
