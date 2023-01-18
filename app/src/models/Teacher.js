0fc70c12472453a8a4f109b507772f4510bf8566
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-18 15:28:16 +0900

    [FEAT]교육자 정보 가져오기 API 구현 완료
    - TeacherStorage에서 phoneNumber로 db에서 교육자 정보 가져오는 getTeacherInfo 메소드 생성
    - Teacher model에서 교육자 정보를 갖고오는 getTeaher 메소드 생성

--- app/src/models/Teacher.js
+++ app/src/models/Teacher.js
@@ -6,6 +6,18 @@ class Teacher {
     constructor(body){
         this.body=body;
     }
+    //교육자 정보 갖고 오기
+    async getTeacher(){
+        try{
+            const client = this.body;
+            const response = await TeacherStorage.getTeacherInfo(client);
+            return response;
+        }catch(err){
+            return {success:false, err};
+        }
+    }
+    
+    //교육자 회원가입 
     async register(){
         const client = this.body;
         try{
@@ -15,7 +27,7 @@ class Teacher {
             return {success : false, err};
         }
     }
-
+    //교육자 프로필 수정하기
     async updateProfile(){
         const client = this.body;
         console.log(client);
--- app/src/models/TeacherStorage.js
+++ app/src/models/TeacherStorage.js
@@ -3,11 +3,11 @@
 const db=require("../config/db");
 
 class TeacherStorage{
-      //phoneNumber로 회원 정보 가져오기
+      //phoneNumber로 교육자 정보 가져오기
       static async getTeacherInfo(phoneNumber){
         return new Promise(async(resolve,reject)=>{
             try{
-                const teacherRef =db.collection("teachers").doc(phoneNumber);
+                const teacherRef =  db.collection("teachers").doc(phoneNumber);
                 const response = await teacherRef.get();
                 resolve(response.data());
             }catch(err){
@@ -15,9 +15,32 @@ class TeacherStorage{
             }
 
         })
-       
     }
-    //선생님 프로필 정보 저장하기 (닉네임, 프로필 정보, 프로필 사진)
+
+     //교육자 정보 저장하기
+     static async save(teacherInfo){
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const phoneNumber=teacherInfo.phoneNumber;
+                const TeacherJson={
+                    phoneNumber:teacherInfo.phoneNumber,
+                    nickName:teacherInfo.nickName,
+                    psword:teacherInfo.psword,
+                    isStudent:teacherInfo.isStudent,
+                    //profileDesc:teacherInfo.profileDesc,
+                    //profilePicture:teacherInfo.profilePicture,
+                    //하트게시물
+                };
+                await db.collection("teachers").doc(phoneNumber).set(TeacherJson);
+                resolve({success:true});
+            } catch(error){
+                reject(`${error}`)
+            }     
+        })
+    }
+
+
+    //교육자 프로필 정보 저장하기 (닉네임, 프로필 정보, 프로필 사진)
     static async saveProfile(profileInfo){
         return new Promise(async(resolve,reject)=>{
             try{
@@ -38,27 +61,6 @@ class TeacherStorage{
     }
 
 
-     //선생님 정보 저장하기
-    static async save(teacherInfo){
-        return new Promise(async(resolve,reject)=>{
-            try{
-                const phoneNumber=teacherInfo.phoneNumber;
-                const TeacherJson={
-                    phoneNumber:teacherInfo.phoneNumber,
-                    nickName:teacherInfo.nickName,
-                    psword:teacherInfo.psword,
-                    isStudent:teacherInfo.isStudent,
-                    //profileDesc:teacherInfo.profileDesc,
-                    //profilePicture:teacherInfo.profilePicture,
-                    //하트게시물
-                };
-                await db.collection("teachers").doc(phoneNumber).set(TeacherJson);
-                resolve({success:true});
-            } catch(error){
-                reject(`${error}`)
-            }     
-        })
-    }
 
 
 }
