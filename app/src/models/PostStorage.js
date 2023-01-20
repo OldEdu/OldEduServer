0b63775fbb7a1a94a5b1953f9e9cb4c35d7810f4
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-20 13:50:14 +0900

    [FEAT]게시글 등록 기능 구현 완료
    -Post,PostStorage 모델 생성

---
+++ app/src/models/PostStorage.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const db= require("../config/db");
+
+//PostStorage에서는 DB를 CRUD(생성,읽기,수정,삭제)역할
+class PostStorage{
+    
+    //postID로 게시글 정보 갖고오기 
+    static async getPostInfo(postInfo){
+        return new Promise(async(resolve, reject)=>{
+            try{
+                const postRef = db.collection("eduPost").doc(postInfo.postID);
+                const response =await postRef.get();
+                resolve(response.data());
+            }catch(err){
+                reject(`${err}`);
+            }
+        })
+    }
+
+    //userID로 내가 쓴 게시글 갖고오기
+    static async getPosts(userID){
+        return new Promise(async(resolve,reject)=>{
+            try{
+                const postRef = db.collection("eduPost")
+                var posts = postRef.orderByChild("userID").equalTo(userID);
+                posts.once('value',function(data){
+                    console.log(data.val());
+                })
+            }
+        })
+    }
+
+
+
+    //게시글 등록하기
+    static async save(postInfo){
+        return new Promise(async(resolve, reject)=>{
+            try{
+                const in_date=new Date().toLocaleString(); //게시글 등록 시 날짜 및 시간
+                const postJson={
+                    title:postInfo.title,
+                    category:postInfo.category,
+                    in_date:in_date,
+                    views:0,
+                    heart:0,
+                    declaration:0,
+                    userID:postInfo.userID,
+                }
+                await db.collection("eduPost").push().set(postJson);
+                resolve({success:true});
+            }catch(err){
+                reject(`${err}`);
+            }
+        })
+    }
+}
