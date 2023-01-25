b2db3d170c6ae60230136d76060c1e6c4efca4f6
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-01-25 15:21:34 +0900

    [FEAT]하트가 가장 많은 게시글 불러오기 기능 라우팅
    - 하트가 가장 많은 게시글 불러오기 기능 라우팅
    - /hearPosts

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -11,7 +11,8 @@ router.get("/register",ctrl.output.register);
 router.get("/profile/:userID",ctrl.output.profile);
 router.get("/myPost/:userID",ctrl.output.myPost);
 router.get("/post/:postID",ctrl.output.post);
-router.get("/recentPost",ctrl.output.recentPost);
+router.get("/recentPosts",ctrl.output.recentPosts);
+router.get("/heartPosts",ctrl.output.heartPosts);
 
 router.post("/login",ctrl.process.login);
 router.post("/register",ctrl.process.register);
