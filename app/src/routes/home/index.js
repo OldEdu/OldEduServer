ea4837541915aaa83b20598d619d0992a8f83a35
Jihyeon1520 <82721608+hyeon1520@users.noreply.github.com>
2023-01-28 01:32:10 +0900

    [FEAT] 스크랩 저장 기능 생성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,9 +9,11 @@ router.get("/",ctrl.output.home);
 router.get("/login",ctrl.output.login);
 router.get("/register",ctrl.output.register);
 router.get("/profile/:userID",ctrl.output.profile);
+// router.get("/scrap/:userID",ctrl.output.scrap);
 
 
 router.post("/login",ctrl.process.login);
 router.post("/register",ctrl.process.register);
 router.post("/profile/:userID",ctrl.process.profile);
+router.post("/scrap",ctrl.process.scrap);
 module.exports=router;
