fc85b4c3ffc11784a6e7ba98b96a8a20f14de1da
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-02-02 14:58:30 +0900

    [FEAT]교육사진 작성,수정,삭제 기능 구현
    - 교육사진 작성,수정,삭제 기능 구현

---
+++ app/src/models/EduPhoto.js
@@ -0,0 +1,45 @@
+"use strict";
+const { eduPhoto_num } = require("./EduPhotoStorage");
+const EduPhotoStorage = require("./EduPhotoStorage");
+//EduPhoto은 게시글 데이터를 가지고 검증 및  조작하는 역할
+
+class EduPhoto{
+    constructor(body){
+        this.body=body;
+    }
+    
+     //교육사진 등록 기능
+     async createEduPhoto(){
+        const client =this.body;
+        try{
+            const response = await EduPhotoStorage.saveEduPhoto(client);
+            return response;
+        }catch(err){
+            return {succrss: false,err};
+        }
+
+     }
+     //교육사진 수정하기 기능
+    async updateEduPhoto(){
+        const client =this.body;
+        try{
+            const response = await EduPhotoStorage.updateEduPhoto(client);
+            return response
+        }catch(err){
+            return {success:false,err};
+        }
+    }
+    //교육사진 삭제 기능
+    async deleteEduPhoto(eduPhotoID){
+        try{
+            const response= await EduPhotoStorage.deleteEduPhoto(eduPhotoID);
+            return response;
+        }catch(err){
+            return {success:false,err};
+        }
+    }
+
+     
+}
+
+module.exports=EduPhoto
