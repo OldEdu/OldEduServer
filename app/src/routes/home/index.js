75e1b59cc787a11fd4e2e8049fd6f7244750da63
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-02-07 11:34:10 +0900

    [FEAT]postID를 이용해 eduPost에 추가된 모든 교육사진 정보 불러오기 기능 라우팅
    - request : get
    - /eduPhoto/:post

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -11,6 +11,8 @@ router.get("/register",ctrl.output.register);
 router.get("/profile/:userID",ctrl.output.profile);
 router.get("/myPost/:userID",ctrl.output.myPost);
 router.get("/post/:postID",ctrl.output.post);
+router.get("/eduPhoto/:postID",ctrl.output.eduPhoto);
+
 router.get("/recentPosts",ctrl.output.recentPosts);
 router.get("/heartPosts",ctrl.output.heartPosts);
 router.get("/viewsPosts",ctrl.output.viewsPosts);
