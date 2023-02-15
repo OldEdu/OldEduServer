792053a54db0741b52ef63eee85b14d1cf8a66d7
Jihyeon1520 <82721608+hyeon1520@users.noreply.github.com>
2023-02-15 13:16:15 +0900

    [FEAT] 댓글 기능 추가
    댓글 작성 / 특정 게시글의 댓글 목록 불러오기 / 특정 유저의 댓글 목록 불러오기

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,9 +1,10 @@
 "use strict"
-const db=require("../../config/db");
-const User=require("../../models/User");
-const Teacher=require("../../models/Teacher");
+const db = require("../../config/db");
+const User = require("../../models/User");
+const Teacher = require("../../models/Teacher");
 const Student = require("../../models/Student");
-const Post =require("../../models/Post");
+const Post = require("../../models/Post");
+const Comment = require("../../models/Comment");
 
 const output={
     home : (req,res)=>{
@@ -12,19 +13,18 @@ const output={
     login : (req,res)=>{
         res.render("home/login");
     },
-    register:(req,res)=>{
+    register : (req,res)=>{
         res.render("home/register");
     },
-
     profile:async(req,res)=>{
         try{
             var userType = await new User(req.body).getUserType(req.params.userID);
-            if(userType ===true){ //학습자인 경우 => 학습자 정보 불러오기
-                const student =new Student(req.params.userID);
+            if(userType === true){ // 학습자인 경우 => 학습자 정보 불러오기
+                const student = new Student(req.params.userID);
                 const response = await student.getStudent();
                 res.send(response);
             }
-            else { //교육자 정보 불러오기
+            else { // 교육자 정보 불러오기
                 const teacher = new Teacher(req.params.userID);
                 const response = await teacher.getTeacher();
                 res.send(response);
@@ -35,35 +35,35 @@ const output={
 
     },
     myPost:async(req,res)=>{
-        const posts= new Post(req.params.userID);
+        const posts = new Post(req.params.userID);
         const response =await posts.readMyPostAll();
         res.send(response);
     },
     post:async(req,res)=>{
-        const posts=new Post(req.params.postID);
+        const posts = new Post(req.params.postID);
         const response =await posts.readPost();
         res.send(response);
     },
     recentPosts:async(req,res)=>{
-        const posts=new Post();
+        const posts = new Post();
         const response = await posts.readPostAll();
         res.send(response);
-
     },
     heartPosts:async(req,res)=>{
-        const posts=new Post();
+        const posts = new Post();
         const response = await posts.readHeartPostAll();
         res.send(response);
-
     },
     viewsPosts:async(req,res)=>{
-        const posts=new Post();
+        const posts = new Post();
         const response = await posts.readViewPostAll();
         res.send(response);
-
     },
-
-
+    writeComment:async(req,res)=>{
+        const posts = new Post();
+        const response = await comment.writeComment();
+        res.send(response);
+    },
 }
 
 const process={
@@ -86,8 +86,27 @@ const process={
         const post = new Post(req.body);
         const response = await post.createPost();
         return res.json(response);
+    },
+    writeComment:async(req,res)=>{
+        const comment = new Comment(req.body);
+        const response = await comment.writeComment();
+        return res.json(response);
+    },
+    commentListPost:async(req,res)=>{
+        const comment = new Comment(req.body);
+        const response = await comment.getComment_Post();
+        return res.json(response);
+    },
+    commentListUser:async(req,res)=>{
+        const comment = new Comment(req.body);
+        const response = await comment.getComment_User();
+        return res.json(response);
+    },
+    updateComment:async(req,res)=>{
+        const comment = new Comment(req.body);
+        const response = await comment.updateComment();
+        return res.json(response);
     }
-
 };
 
 module.exports={
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -19,4 +19,9 @@ router.post("/login",ctrl.process.login);
 router.post("/register",ctrl.process.register);
 router.post("/profile/:userID",ctrl.process.profile);
 router.post("/createPost",ctrl.process.createPost);
+router.post("/writeComment",ctrl.process.writeComment);
+router.post("/commentListPost",ctrl.process.commentListPost);
+router.post("/commentListUser",ctrl.process.commentListUser);
+router.post("/updateComment",ctrl.process.updateComment);
+
 module.exports=router;
