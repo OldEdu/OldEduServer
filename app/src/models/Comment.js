199d8add534161660cf00fdc46604319cb0f6724
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-03-15 16:57:38 +0900

    [REFACTOR]로그인 했을 경우 자신이 작성한 댓글인지 확인 할 수 있는 myComment필드 추가
    -로그인 했을 경우 postID에 맞는 댓글 리스트 가져오기, userID가 쓴 댓글일 경우에는 myComment값 true, 안 쓴 댓글 일 경우에는 false를 나타내는 필드 추가
    -로그인하지 않았을 경우 myComment값 모두 false

--- app/src/models/Comment.js
+++ app/src/models/Comment.js
@@ -18,11 +18,21 @@ class Comment{
         }
     }
 
-    // 특정 게시글의 댓글들 불러오기
+    // 로그인 하지 않았을 경우 특정 게시글의 댓글들 불러오기
     async getComment_Post(){
         const client = this.body; // postID 가져오기
         try{
             const response = await CommentStorage.getCommentList_Post(client);
+            return response;
+        }catch(err){
+            return {success:false,err};
+        }
+    }
+
+    // 로그인 했을 경우 특정 게시글의 댓글들 불러오기
+    async getComment_Post_Login(postID,userID){
+        try{
+            const response = await CommentStorage.getCommentList_Post_Login(postID,userID);
             return response;
         }catch(err){
             return {success:false,err};
