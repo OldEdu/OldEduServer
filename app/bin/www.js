603be0a81d85d17725b2624c24cc1b157c9d9035
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2023-03-27 01:08:07 +0900

    [FEAT]Algorlia와 Firestore 연동 및 Function등록
    - Algorlia와 Firestore 연동
    eduPost 컬렉션 db Algorlia search와 연동
    - Firebase-Function등록
    sendCollectionToAlgolia: Algolia에 eduPost 컬렉션 db 업로드(1회만 실행하면 됨)
    collectionOnCreate:eduPost에 추가되면 Algolia에도 데이터 추가됨
    ticcleOnUpdate:eduPost 필드(title,category)바뀌면 Algolia에도 데이터 변경됨
    ticcleOnDelete:eduPost 삭제되면 Algolia에도 데이터 삭제됨

--- app/bin/www.js
+++ app/bin/www.js
@@ -1,8 +1,8 @@
 "use strict"
 
 const app = require("../app");
-
 const PORT =process.env.PORT||8080;
-app.listen(PORT,()=>{
+
+app.listen(PORT,async ()=>{
     console.log(`Server is running on PORT ${PORT}.`);
 })
